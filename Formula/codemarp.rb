c435714bef37201549196e61b52f98e1b0e9c980
haddyadnan <haddyadnan@gmail.com>
2026-04-27 01:16:11 +0100

    bump version to 0.4.2

--- Formula/codemarp.rb
+++ Formula/codemarp.rb
@@ -3,8 +3,8 @@ class Codemarp < Formula
 
   desc "Visualize code architecture, call graphs, and control flow"
   homepage "https://github.com/haddyadnan/codemarp"
-  url "https://github.com/haddyadnan/codemarp/archive/refs/tags/v0.3.2.tar.gz"
-  sha256 "ca373b7db52741351d8f853c2fed81eaccf36b08983fd4760dacca408964903e"
+  url "https://github.com/haddyadnan/codemarp/archive/refs/tags/v0.4.2.tar.gz"
+  sha256 "ee613843671fec9087e5566c3d9d660eed8aa89e22b9cc33fd9c13892a0d90e3"
   license "MIT"
 
   depends_on "python@3.12"
