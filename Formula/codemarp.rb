7fd828239d9ae6056172ba45f92624b9d960f826
haddyadnan <haddyadnan@gmail.com>
2026-04-28 07:39:33 +0100

    move tree sitter python to wheel based

--- Formula/codemarp.rb
+++ Formula/codemarp.rb
@@ -14,12 +14,12 @@ class Codemarp < Formula
     sha256 "fe43c158555da46723b28b52e058ad444195afd1db3ca7720c59a254544e9c20"
   end
 
-  resource "tree-sitter-python" do
-    url "https://files.pythonhosted.org/packages/b8/8b/c992ff0e768cb6768d5c96234579bf8842b3a633db641455d86dd30d5dac/tree_sitter_python-0.25.0.tar.gz"
-    sha256 "b13e090f725f5b9c86aa455a268553c65cadf325471ad5b65cd29cac8a1a68ac"
-  end
-
   on_arm do
+    resource "tree-sitter-python" do
+      url "https://files.pythonhosted.org/packages/e6/1d/60d8c2a0cc63d6ec4ba4e99ce61b802d2e39ef9db799bdf2a8f932a6cd4b/tree_sitter_python-0.25.0-cp310-abi3-macosx_11_0_arm64.whl"
+      sha256 "480c21dbd995b7fe44813e741d71fed10ba695e7caab627fb034e3828469d762"
+    end
+
     resource "tree-sitter-typescript" do
       url "https://files.pythonhosted.org/packages/8f/2f/1f36fda564518d84593f2740d5905ac127d590baf5c5753cef2a88a89c15/tree_sitter_typescript-0.23.2-cp39-abi3-macosx_11_0_arm64.whl"
       sha256 "c7cc1b0ff5d91bac863b0e38b1578d5505e718156c9db577c8baea2557f66de8"
@@ -27,6 +27,11 @@ class Codemarp < Formula
   end
 
   on_intel do
+    resource "tree-sitter-python" do
+      url "https://files.pythonhosted.org/packages/cf/64/a4e503c78a4eb3ac46d8e72a29c1b1237fa85238d8e972b063e0751f5a94/tree_sitter_python-0.25.0-cp310-abi3-macosx_10_9_x86_64.whl"
+      sha256 "14a79a47ddef72f987d5a2c122d148a812169d7484ff5c75a3db9609d419f361"
+    end
+
     resource "tree-sitter-typescript" do
       url "https://files.pythonhosted.org/packages/28/95/4c00680866280e008e81dd621fd4d3f54aa3dad1b76b857a19da1b2cc426/tree_sitter_typescript-0.23.2-cp39-abi3-macosx_10_9_x86_64.whl"
       sha256 "3cd752d70d8e5371fdac6a9a4df9d8924b63b6998d268586f7d374c9fba2a478"
@@ -37,13 +42,14 @@ class Codemarp < Formula
     venv = virtualenv_create(libexec, "python3.12")
 
     venv.pip_install resource("tree-sitter")
-    venv.pip_install resource("tree-sitter-python")
 
-    resource("tree-sitter-typescript").stage do
-      wheel = Dir["*.whl"].first
-      raise "tree-sitter-typescript wheel not found" if wheel.nil?
+    ["tree-sitter-python", "tree-sitter-typescript"].each do |name|
+      resource(name).stage do
+        wheel = Dir["*.whl"].first
+        raise "#{name} wheel not found" if wheel.nil?
 
-      system libexec/"bin/python", "-m", "pip", "install", "--no-deps", "--no-index", Pathname.pwd/wheel
+        system libexec/"bin/python", "-m", "pip", "install", "--no-deps", "--no-index", Pathname.pwd/wheel
+      end
     end
 
     venv.pip_install_and_link buildpath
